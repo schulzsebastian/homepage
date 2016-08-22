62e161396a115111b0dc14daca28d8bee3a25633
schulzsebastian <schulz.siwy@gmail.com>
2016-08-23 00:02:12 +0200

    first section

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Homepage', 'Skills', 'Interests', 'Contact']
+  });
+});
