0bb0e9198d9742e3dec04d49316451375dcc5cf0
Sebastian Schulz <schulz.siwy@gmail.com>
2016-09-28 13:50:32 -0400

    server side

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([52.482780222078226, 18.676757812500004], 3);
-var osm =  L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+var osm =  L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
   attribution: 'Travel map by Sebastian Schulz &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
 var googleMaps = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
