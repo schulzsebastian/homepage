769a78c184301c8e80ae6949350b3bf265f5ead2
Sebastian Schulz <schulz.siwy@gmail.com>
2016-11-07 00:16:57 +0100

    repository + plugin

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # !usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
@@ -9,6 +9,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/qgis_repository")
+def qgis_repository():
+    repository = make_response(render_template("qgis_repository.xml"))
+    repository.headers["Content-Type"] = "application/xml"
+    return repository
+
+
+@app.route("/qgisplugin_spreadsheet")
+def qgisplugin_spreadsheet():
+    return app.send_static_file('repository/qgisplugin_spreadsheet.zip')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
